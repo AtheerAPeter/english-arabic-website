d321feaac824d1644cbbf5231b78506501323e0e
atheer <atheer@pop-os.localdomain>
2020-11-01 17:57:24 +0300

    first commit

---
+++ content/homeContent.js
@@ -0,0 +1,7 @@
+export default {
+  en: {
+    title: "hello all",
+    msg: "hello fikracamps",
+  },
+  ar: { title: "مرحبا جميعا", msg: "مرحبا فكراكامبس" },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import "../styles/rtl.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My website</title>
+        <meta property="og:title" content="my website" />
+        <meta property="og:description" content="my website description" />
+        <meta property="og:image" content="https://picsum.photos/300/300" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
